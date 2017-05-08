0367c9aece4108c665164ab10a2ae62ebc84b60e
Kuniaki IGARASHI <igaiga@gmail.com>
2017-05-08 16:56:57 +0900

    Writing the book model spec

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -5,14 +5,21 @@ RSpec.describe Book, type: :model do
     it { expect(Book.all).to be_empty}
   end
   describe "#echo" do
-    context "boolean true" do
-      subject { Book.echo(true) }
-      it { is_expected.to be_truthy}
+    context "Boolean" do
+      shared_examples_for "boolean" do
+        subject { Book.echo(boolean_value) }
+        it { is_expected.to eq(boolean_value)}
+      end
+      describe "true" do
+        let(:boolean_value){ true }
+        it_behaves_like "boolean"
+      end
+      describe "false" do
+        let(:boolean_value){ false }
+        it_behaves_like "boolean"
+      end
     end
-    context "boolean false" do
-      subject { Book.echo(false) }
-      it { is_expected.to be_falsy}
-    end
+
     context "Interger" do
       it { expect(Book.echo(0)).to be_zero}
       it { expect(Book.echo(1)).to eq(1)}
